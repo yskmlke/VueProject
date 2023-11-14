9e556fe2ed85cd4cef83216e76d86bea4063a67b
yskmlke <2212586272@qq.com>
2023-11-14 21:54:08 +0800

    test 3

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -12,16 +12,27 @@ export default {
       try {
         const stream = await navigator.mediaDevices.getUserMedia({ video: { facingMode: 'environment' } });
         const track = stream.getVideoTracks()[0];
+        
+        if (!track) {
+          alert('未找到摄像头轨道，可能设备不支持闪光灯');
+          return;
+        }
+
         const capabilities = track.getCapabilities();
 
-        if (capabilities.torch) {
-          await track.applyConstraints({ advanced: [{ torch: true }] });
-          alert('闪光灯已打开');
-        } else {
+        if (!capabilities.torch) {
           alert('设备不支持闪光灯');
+          return;
         }
+
+        await track.applyConstraints({ advanced: [{ torch: true }] });
+        alert('闪光灯已打开');
       } catch (error) {
-        alert('无法访问摄像头或用户拒绝了访问权限');
+        if (error.name === 'NotAllowedError' || error.name === 'NotFoundError') {
+          alert('无法访问摄像头或用户拒绝了访问权限');
+        } else {
+          alert('发生未知错误: ' + error.message);
+        }
       }
     },
   },
